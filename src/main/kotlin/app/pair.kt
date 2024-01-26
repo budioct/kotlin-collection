4648c65f92738a901c62739c47659aa39f91052b
Budhi Octaviansyah <budioct@gmail.com>
2024-01-26 09:13:04 +0700

    pair

---
+++ src/main/kotlin/app/pair.kt
@@ -0,0 +1,16 @@
+package app
+
+fun main() {
+
+    // Pair adalah representasi dari data yang berpasangan
+    // Di Map, Pair digunakan sebagai data Map ketika pertama kali Map dibuat
+
+    val pair1: Pair<String, String> = Pair("budhi", "oct")
+    println(pair1.first)
+    println(pair1.second)
+
+    val pair2: Pair<String, String> = Pair("sadam", "husein")
+    println(pair2.first)
+    println(pair2.second)
+
+}
